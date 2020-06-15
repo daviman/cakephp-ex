3ebe549553f561c73af01d23c9a20d66d1750b75
daviman <daviman@gmail.com>
2020-06-15 15:50:34 -0700

    fixed ver

--- openshift/templates/create-cakephp.sh
+++ openshift/templates/create-cakephp.sh
@@ -4,7 +4,9 @@ oc new-app --template ${RHT_OCP4_DEV_USER}-common/cakephp-example \
 	-p NAME=cakephpex \
 	-p APPLICATION_DOMAIN=quote-${RHT_OCP4_DEV_USER}.${RHT_OCP4_WILDCARD_DOMAIN} \
 	-p SOURCE_REPOSITORY_URL=https://github.com/${RHT_OCP4_GITHUB_USER}/cakephp \
-	-p SOURCE_REPOSITORY_REF=devenv0 \
+	-p SOURCE_REPOSITORY_REF=devenv01 \
 	--name cakephpex
+	--source-secret=github-secret
+
 
 
