8f13edb11d9925003bfa47ea532390f93fe5267d
daviman <daviman@gmail.com>
2020-06-16 07:22:21 -0700

    Making test changes

--- openshift/templates/create-cakephp.sh
+++ openshift/templates/create-cakephp.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-oc new-app --template ${RHT_OCP4_DEV_USER}-common/cakephp-example \
+oc new-app --template ${RHT_OCP4_DEV_USER}-dev00-common/cakephp-example \
 	-p NAME=cakephpex \
 	-p APPLICATION_DOMAIN=quote-${RHT_OCP4_DEV_USER}.${RHT_OCP4_WILDCARD_DOMAIN} \
 	-p SOURCE_REPOSITORY_URL=https://github.com/${RHT_OCP4_GITHUB_USER}/cakephp \
 	-p SOURCE_REPOSITORY_REF=devenv01 \
-	--name cakephpex \
-	--source-secret=fb79ca16253b6ae9b56471d85d41d98e7c27c7ec
+	--name cakephpex 
 
 
 
